fb2fd648774313635fe20f48e2a0e1783018392a
Ian McCormack <icmccorm@cs.cmu.edu>
2026-02-24 17:48:11 -0500

    Disabled leak checking.

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ cargo build --release
 cargo bsan build
 RUNS=100
 WARMUP=10
-MFLAGS="-Zmiri-deterministic-concurrency -Zmiri-disable-validation -Zmiri-disable-alignment-check -Zmiri-disable-data-race-detector -Zmiri-tree-borrows -Zmiri-provenance-gc=0"
+MFLAGS="-Zmiri-deterministic-concurrency -Zmiri-disable-validation -Zmiri-disable-alignment-check -Zmiri-disable-data-race-detector -Zmiri-tree-borrows -Zmiri-ignore-leaks -Zmiri-provenance-gc=0"
 # we need to override the ABI warning due to what seems to be a bug in nightly (https://github.com/mozilla/cubeb-coreaudio-rs/issues/269)
 CARGO_TARGET_DIR=./target/asan RUSTFLAGS="-Cunsafe-allow-abi-mismatch=sanitizer -Zsanitizer=address --target=x86_64-unknown-linux-gnu" RUSTDOCFLAGS=$RUSTFLAGS cargo build -Zbuild-std
 CARGO_TARGET_DIR=./target/tsan RUSTFLAGS="-Cunsafe-allow-abi-mismatch=sanitizer -Zsanitizer=thread --target=x86_64-unknown-linux-gnu" RUSTDOCFLAGS=$RUSTFLAGS cargo build -Zbuild-std
